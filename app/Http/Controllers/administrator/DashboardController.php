15e4cf1f3d8462d12632c9b1cc93ca65c7ea80ac
sanbray99 <ikhsannawawi99@gmail.com>
2023-08-15 01:49:18 +0700

    membuat controller dashboard

---
+++ app/Http/Controllers/administrator/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\administrator;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        return view('administrator.dashboard.index');
+    }
+}
